83a436b771e79af6619c115c7be84600bb509946
Marcus Vinicius <marcus.vpv2@gmail.com>
2024-10-30 23:26:57 -0300

    30/10/24 - feat: Funcionalidade do formulário de criar um novo job, página de ver vaga e o seu css

--- app.js
+++ app.js
@@ -1,51 +1,69 @@
-const express = require('express');
-const {engine} = require('express-handlebars');
+const express = require("express");
+const { engine } = require("express-handlebars");
 const app = express();
-const path = require('path');
-const db = require('./db/connection');
-const bodyParser = require('body-parser');
-const Job = require('./models/Job');
+const path = require("path");
+const db = require("./db/connection");
+const bodyParser = require("body-parser");
+const Job = require("./models/Job");
+const { where } = require("sequelize");
+const Sequelize = require('sequelize');
+const Op = Sequelize.Op;
 
 const PORT = 3000;
 
-app.listen(PORT, function() {
-    console.log(`Express rodando na porta ${PORT}`)
+app.listen(PORT, function () {
+  console.log(`Express rodando na porta ${PORT}`);
 });
 
 //body parser
 app.use(bodyParser.urlencoded({ extended: false }));
 
 //handlebars
-app.set('views', path.join(__dirname, 'views'));// indica o diretório base dos layouts na pasta views
-app.engine('handlebars', engine({defaultLayout: 'main'}));// define o layout principal
-app.set('view engine', 'handlebars');// define a biblioteca para redenrizar as views
+app.set("views", path.join(__dirname, "views")); // indica o diretório base dos layouts na pasta views
+app.engine("handlebars", engine({ defaultLayout: "main" })); // define o layout principal
+app.set("view engine", "handlebars"); // define a biblioteca para redenrizar as views
 
 //static folder
-app.use(express.static(path.join(__dirname, 'public')));//indica qual é a pasta de arquivos estáticos 
+app.use(express.static(path.join(__dirname, "public"))); //indica qual é a pasta de arquivos estáticos
 
 //db connection
-db
-    .authenticate()
-    .then(() => {
-
-        console.log('Conectou ao banco com sucesso!')
-    })
-    .catch(err => {
-        console.log('Ocorreu um erro ao conectar: ',err)
-    });
+db.authenticate()
+  .then(() => {
+    console.log("Conectou ao banco com sucesso!");
+  })
+  .catch((err) => {
+    console.log("Ocorreu um erro ao conectar: ", err);
+  });
 
 //routs
-app.get('/', (req, res) => {
-    Job.findAll({order: [// Chama todos os jobs e ordena por data de criação decrescente
-        ['createdAt', 'DESC']
-    ]})
-    .then(jobs => {
+app.get("/", (req, res) => {
+    let search = req.query.job;
+    let query = '%'+search+'%';
 
-        res.render('index', {
-            jobs
-        });
-    });
-})
+    if (!search) {
+        Job.findAll({order: [
+            ["createdAt", "DESC"]// Chama todos os jobs e ordena por data de criação decrescente
+        ]})
+        .then(jobs => {
+            res.render("index", {
+                jobs,
+            });
+        })
+        .catch(err => console.log(err));
+    } else {
+        Job.findAll({
+            where: {title: {[Op.like]: query}},
+            order: [
+            ["createdAt", "DESC"]// Chama todos os jobs e ordena por data de criação decrescente
+        ]})
+        .then(jobs => {
+            res.render("index", {
+                jobs, search
+            });
+        })
+        .catch(err => console.log(err));
+    }
+});
 
 // jobs routs
-app.use('/jobs', require('./routes/jobs'));
+app.use("/jobs", require("./routes/jobs"));
